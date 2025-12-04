d8d2b7559dbb7fac27b22032e373a738165f4aca
LarsBecker381 <larsbecker300@gmail.com>
2025-12-04 12:31:31 +0100

    Eingabe

--- RDG.cs
+++ RDG.cs
@@ -1,10 +1,50 @@
 ﻿using System;
 
-public class Class1
+namespace Projekt
 {
-	public Class1()
-	{
-        //hikfhnrkf
-        Console.WriteLine("Hello world");
-	}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int breite = 0;
+            int hoehe = 0;
+
+            do
+            {
+                try
+                {
+
+                    if (breite == 0)
+                    {
+                        Console.WriteLine("Bitte die Breite Eingeben!");
+                        breite = Convert.ToInt32(Console.ReadLine());
+
+                        if (breite < 10 || breite > 50)
+                        {
+                            throw new ArgumentException("\nDie Breite muss größer als 10 und kleiner als 50 sein .\n");
+                        }
+
+                    }
+
+                    Console.WriteLine("Bitte die Höhe Eingeben!");
+                    hoehe = Convert.ToInt32(Console.ReadLine());
+
+
+                    if (hoehe < 10 || hoehe > 25)
+                    {
+                        throw new ArgumentException("\nDie Höhe muss größer als 10 und kleiner als 25 sein.\n");
+                    }
+                    break;
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+
+                }
+
+            }
+            while (true);
+
+        }
+    }
 }
